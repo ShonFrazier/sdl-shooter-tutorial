6332230e23ce04065aa029a3cc5ebc157811dca6
Shon Frazier <code@shon.io>
2024-10-18 21:05:55 -0400

    tidy #includes in common.h

--- src/common.h
+++ src/common.h
@@ -4,11 +4,11 @@
 
 #pragma once
 
-#include "ctype.h"
+#include <math.h>
+#include <SDL2/SDL.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 #include "defs.h"
-#include "math.h"
-#include "SDL2/SDL.h"
-#include "stdio.h"
-#include "stdlib.h"
-#include "string.h"
 #include "structs.h"
